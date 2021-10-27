2c8ab5359f900fcdb5fca2c079926a80159745d3
leocoder <leocoderV@gmail.com>
2021-10-27 23:05:58 +0800

    2021-10-27

---
+++ src/com/sike/SynchronizedTest.java
@@ -0,0 +1,21 @@
+package com.sike;
+
+import java.util.ArrayList;
+
+public class SynchronizedTest {
+
+    public synchronized void test1(){
+
+    }
+
+    public void test2(){
+        synchronized (this){
+
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.printf("hello world");
+        ArrayList
+    }
+}
